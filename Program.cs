c54335cdb239c2a02a296a93c6b5989a7a590931
yaroshevski <wsr1@K218PC1>
2022-04-12 10:03:38 +0300

    Задание 7

--- Program.cs
+++ Program.cs
@@ -7,12 +7,24 @@ namespace ZadanieX
         static void Main(string[] args)
         {
             int h = 1;
+            //-------------------------------------------------------------
             double k = 999.01;
+            //-------------------------------------------------------------
             var s = 10;
+            //-------------------------------------------------------------
             Console.WriteLine($"{h} {k} {s}");
+            //-------------------------------------------------------------
             h = h * h;
             Console.WriteLine(h);
+            //-------------------------------------------------------------
             Console.WriteLine(12 % 4);
+            //-------------------------------------------------------------
+            Console.WriteLine("Введите переменные a, b, c");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a * b * c);
+            Console.ReadKey();
         }
     }
 }
